2bf3ac5a8fc31938f4cfe3119630f48a6cfd8e52
Yelena <y.puentes@uniandes.edu.co>
2025-11-02 21:39:22 -0500

    Cambios

--- wms/wms_producer.py
+++ wms/wms_producer.py
@@ -1,18 +1,19 @@
 import requests
 import time
-import mysql.connector
+import pymysql
 
 ERP_URL = "http://54.226.55.197:5000/sync"
+
 DB_CONFIG = {
     "host": "wms-db.cn2k0uwymh6v.us-east-1.rds.amazonaws.com",
     "user": "admin",
     "password": "admin1234...",
     "database": "wms-db",
-    "ssl_disabled": True 
+    "connect_timeout": 5,
+    "ssl": {"ssl": False}  # fuerza sin SSL
 }
 
 def enviar_pedido(pedido_id):
-    """Envía un pedido al ERP. Si falla o se demora >0.9 s, lo guarda en la cola local."""
     inicio = time.time()
     try:
         r = requests.get(ERP_URL, timeout=0.9)
@@ -22,7 +23,7 @@ def enviar_pedido(pedido_id):
         if r.status_code == 200:
             print(f"[WMS] Pedido {pedido_id} procesado correctamente en el ERP.")
         else:
-            print(f"[ERROR] ERP devolvió estado {r.status_code}, encolando pedido {pedido_id}.")
+            print(f"[ERROR] ERP devolvió {r.status_code}. Encolando pedido {pedido_id}.")
             guardar_en_cola(pedido_id)
 
     except Exception as e:
@@ -30,10 +31,10 @@ def enviar_pedido(pedido_id):
         print(f"[FALLO] ERP no respondió en 0.9 s ({round(duracion,3)} s). Encolando pedido {pedido_id}...")
         guardar_en_cola(pedido_id)
 
+
 def guardar_en_cola(pedido_id):
-    """Guarda un pedido en la base de datos local (cola_local) cuando el ERP no responde."""
     try:
-        db = mysql.connector.connect(**DB_CONFIG)
+        db = pymysql.connect(**DB_CONFIG)
         cursor = db.cursor()
         cursor.execute(
             "INSERT INTO cola_local (pedido_id, estado) VALUES (%s, %s)",
@@ -50,6 +51,6 @@ if __name__ == "__main__":
     print("=== WMS Producer iniciado ===")
     print("Enviando pedidos al ERP cada 3 segundos...\n")
     while True:
-        pedido_id = int(time.time()) 
+        pedido_id = int(time.time())
         enviar_pedido(pedido_id)
         time.sleep(3)
