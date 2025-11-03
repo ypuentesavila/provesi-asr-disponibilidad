82cfa2b0710916cb2fbf5f0d7552968eed1e718e
Yelena <y.puentes@uniandes.edu.co>
2025-11-02 21:24:40 -0500

    Cambios

--- worker/worker_subscriber.py
+++ worker/worker_subscriber.py
@@ -1,32 +1,43 @@
-import time, requests, mysql.connector
+import time
+import requests
+import mysql.connector
 
-ERP_URL = "http://<IP_ERP>:5000/sync"
+ERP_URL = "http://54.226.55.197:5000/sync"
+
 DB_CONFIG = {
-    "host": "<IP_DB>",
+    "host": "wms-db.cn2k0uwymh6v.us-east-1.rds.amazonaws.com",
     "user": "admin",
-    "password": "admin123",
-    "database": "wms"
+    "password": "admin1234...",
+    "database": "wms-db"
 }
 
 def procesar_cola():
-    db = mysql.connector.connect(**DB_CONFIG)
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM cola_local WHERE estado='pendiente'")
-    pedidos = cursor.fetchall()
-    for pedido in pedidos:
-        try:
-            r = requests.get(ERP_URL, timeout=1)
-            if r.status_code == 200:
-                cursor.execute("DELETE FROM cola_local WHERE id=%s", (pedido["id"],))
-                db.commit()
-                print(f"[WORKER] Pedido {pedido['id']} reenviado con éxito.")
-            else:
-                print(f"[WORKER] ERP respondió error para {pedido['id']}.")
-        except Exception as e:
-            print("[WORKER] ERP sigue caído, reintentará más tarde.")
-    db.close()
+    try:
+        db = mysql.connector.connect(**DB_CONFIG)
+        cursor = db.cursor(dictionary=True)
+
+        cursor.execute("SELECT * FROM cola_local WHERE estado='pendiente'")
+        pedidos = cursor.fetchall()
+
+        for pedido in pedidos:
+            try:
+                r = requests.get(ERP_URL, timeout=0.9)
+                if r.status_code == 200:
+                    cursor.execute("DELETE FROM cola_local WHERE id=%s", (pedido["id"],))
+                    db.commit()
+                    print(f"[WORKER] Pedido {pedido['id']} reenviado con éxito.")
+                else:
+                    print(f"[WORKER] ERP respondió error para pedido {pedido['id']}.")
+            except Exception as e:
+                print(f"[WORKER] ERP sigue caído, reintentará más tarde.")
+    except mysql.connector.Error as err:
+        print(f"[ERROR] Fallo de conexión a la base de datos: {err}")
+    finally:
+        if 'db' in locals() and db.is_connected():
+            db.close()
 
 if __name__ == "__main__":
+    print("[WORKER] Iniciando proceso de sincronización...")
     while True:
         procesar_cola()
         time.sleep(5)
